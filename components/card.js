9f599caee6459f78e2f768f9c89787b8ec17afa7
miedzinskijakub <47180627+miedzinskijakub@users.noreply.github.com>
2021-03-14 22:43:13 +0100

    api random

--- components/card.js
+++ components/card.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { StyleSheet, Text, View, Image } from 'react-native';
+import Swiper from 'react-native-deck-swiper'
+
+
 
 
 class Card extends React.Component {
@@ -8,12 +11,15 @@ class Card extends React.Component {
       this.state = {
         error: null,
         isLoaded: false,
-        items: []
+        items: [],
+        index: 0,
+        setIndex: 0,
       };
     }
+
   
     componentDidMount() {
-      fetch("https://dog.ceo/api/breed/akita/images/random")
+      fetch("https://dog.ceo/api/breeds/image/random")
         .then(res => res.json())
         .then(
           (result) => {
@@ -31,19 +37,32 @@ class Card extends React.Component {
             });
           }
         )
-    }
+    }            //</View><Image style={styles.images} source={items}/>
+    
+    
+
     render() {
-      const { error, isLoaded, items } = this.state;
+      const { error, isLoaded, items,  } = this.state;
       if (error) {
         return <View>Błąd: {error.message}</View>;
       } else if (!isLoaded) {
         return <Text>Ładowanie...</Text>;
       } else {
-        console.log(items)
+
+        const {container, card, cardImage, textLeft, textRight} = styles;
 
         return (
-          <View style={styles.image}>
-            <Image style={styles.images} source={items}/>
+          <View style={container}>
+            <View style={card}>
+           
+                <Image source={items} style={cardImage}/>
+
+
+            <View>
+              <Text style={textLeft}>Test, 10</Text>
+              <Text style={textRight}>1 Connection</Text>
+            </View>
+            </View>
           </View>
         );
       }
@@ -51,6 +70,36 @@ class Card extends React.Component {
   }
 
   const styles = StyleSheet.create({
+    container:{
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    card:{
+      width: 300,
+      height: 300,
+      padding: 10
+
+    },
+
+    cardImage:{
+      height: 260,
+      borderRadius: 30
+    },
+
+    textLeft:{
+      position: 'absolute',
+      left: 0,
+      top: 0,
+      fontSize: '2rem'
+    },
+
+    textRight:{
+      position: 'absolute',
+      right: 0,
+      top: 0,
+    },
+
     image: {
         width: '100%',
       backgroundColor: '#fff',
